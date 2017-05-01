b0bdb97f7cb384ec4516f470d2712c77367bf072
Blake Geraci <bgeraci851@hcc.edu>
2017-05-01 13:28:13 -0400

    Restructured main such that the command line is now an object

---
+++ src/CommandTable/CommandTable.java
@@ -0,0 +1,166 @@
+package CommandTable;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+public class CommandTable {
+    final String ANSI_RESET = "\u001B[0m";
+    File cwd;
+    boolean stillTakingInput = true;
+    final static String[] VALID_COMMANDS = {"ls", "cd", "cwd", "open", "quit"};
+    
+    public CommandTable(){
+        cwd = new File(System.getProperty("user.dir"));
+    }
+    
+    public CommandTable(File dir){
+        cwd = dir;
+    }
+    
+    public boolean isStillTakingInput(){
+        return stillTakingInput;
+    }
+    
+    public String execute(String command){
+        String cmd = parseCmd(command);
+        
+        if (cmd == null)
+            return "Invalid input.";
+        
+        
+        String returnStatement = "";
+        switch (cmd){
+            case "quit":
+                stillTakingInput = false;
+                break;
+            case "ls":
+                returnStatement = ls();
+                break;
+            case "cd":
+                returnStatement = cd(command);
+                break;
+            case "cwd":
+                returnStatement = cwd.getAbsolutePath();
+                break;
+            case "open":
+                returnStatement = open(command);
+                break;
+            default: 
+                System.out.println("Invalid command.");
+        }
+        return returnStatement;
+    }
+    
+    protected String open(String command){
+        String returnStatement = "";
+        String fileName = cwd.getAbsolutePath() + File.separator + command.replace("open", "").trim();
+        File file = new File(fileName);
+
+        if (file.isFile())
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException ex) {
+                returnStatement = ex.toString();
+            }
+        else 
+            returnStatement = "Invalid file location.";
+        
+        return returnStatement;
+    }
+    
+    protected String cd(String command){
+        boolean validFolder = true;
+        String returnStatement = "";
+        String trimmed = command.replace("cd", "").trim();
+        if (command.contains("..")){
+            cwd = cwd.getParentFile();
+        }
+
+        else if (trimmed.length() == 0){
+            
+            while(cwd.getParentFile() != null) {
+                cwd = cwd.getParentFile();
+            }
+        }    
+        else {
+
+            if (cwd.getAbsolutePath().endsWith(File.separator)){
+                if ((new File(cwd.getAbsolutePath() + trimmed)).isDirectory())
+                    cwd = new File(cwd.getAbsolutePath() + trimmed);
+                else 
+                    validFolder = false;
+            } 
+            else {
+                if ((new File(cwd.getAbsolutePath() + File.separator + trimmed)).isDirectory())
+                    cwd = new File(cwd.getAbsolutePath() + File.separator + trimmed );
+                else 
+                    validFolder = false;
+            }
+        }
+        if (validFolder){
+            returnStatement = cwd.getAbsolutePath();
+        }
+        else 
+            returnStatement = "Invalid folder location.";
+        
+        return returnStatement;
+    }
+    
+    protected String ls (){
+        String text = "";
+        for (String file : cwd.list())
+                    if (!file.startsWith("."))
+                        text += getTypeColor(file) + file + ANSI_RESET + " ";
+        return text;
+    }
+    
+    private String parseCmd(String cmd){
+        String[] truncated = cmd.trim().split(" ");
+        for (String command : VALID_COMMANDS){
+            if (truncated[0].contains(command)){
+                return command;
+            }
+        }
+        return null;
+    }
+    
+    
+    
+    private String getTypeColor(String fileName){
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\u001B[33m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_WHITE = "\u001B[37m";
+        
+        int dotIndex = fileName.lastIndexOf(".");
+        if (dotIndex == -1)
+            return ANSI_BLACK;
+        
+        String type = fileName.substring(dotIndex);
+        
+        switch(type){
+            case ".jpg":
+            case ".png":
+                return ANSI_YELLOW;
+            case ".doc":
+            case ".docx":
+            case ".txt":
+            case ".rtf":
+                return ANSI_BLUE;
+            case ".pdf":
+                return ANSI_RED;
+            case ".xls":
+            case ".xlsx":
+                return ANSI_GREEN;
+            default:
+                return ANSI_BLACK;
+        }        
+   
+       
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+import CommandTable.CommandTable;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
@@ -21,89 +22,22 @@ Current TODOS
 
 public class Main {
     
-    final static String[] VALID_COMMANDS = {"ls", "cd", "cwd", "open", "quit"};
+    
     public static void main(String[] args) throws IOException{
         
+        //File cwd = new File(System.getProperty("user.dir"));
+        CommandTable commandLine = new CommandTable();
         
         Scanner sc = new Scanner(System.in);
-        boolean stillTakingInput = true;
-        File cwd = new File(System.getProperty("user.dir"));
         System.out.println("Working Directory = " +
               System.getProperty("user.dir")+ "\n");
-        while(stillTakingInput){
+        
+        while(commandLine.isStillTakingInput()){
             
             System.out.print(">> ");
             String command = sc.nextLine();
-            String cmd = parseCmd(command);
-            if (cmd == null){
-                System.out.println("Invalid input.");
-                continue;
-            }
             
-            // current issues: 
-            // can't move to bgspiral (not sure why, /Users/)
-            // get a null pointer at Library (/Library)
-            switch (cmd){
-                case "quit":
-                    stillTakingInput = false;
-                    break;
-                case "ls":
-                    for (String file : cwd.list())
-                        if (!file.startsWith("."))
-                            System.out.print(getTypeColor(file) + file + ANSI_BLACK + " ");
-                    System.out.println();
-                    break;
-                case "cd":
-                    boolean validFolder = true;
-                    String trimmed = command.replace("cd", "").trim();
-                    if (command.contains("..")){
-                        cwd = cwd.getParentFile();
-                    }
-                    
-                    else if (trimmed.length() == 0)
-                        while(cwd.getParentFile() != null){
-                            cwd = cwd.getParentFile();
-                        }
-                    else {//((new File(cwd.getAbsolutePath() + trimmed)).isDirectory()) {
-                        
-                        if (cwd.getAbsolutePath().endsWith(File.separator)){
-                            if ((new File(cwd.getAbsolutePath() + trimmed)).isDirectory())
-                                cwd = new File(cwd.getAbsolutePath() + trimmed);
-                            else 
-                                validFolder = false;
-                        } 
-                        else {
-                            if ((new File(cwd.getAbsolutePath() + File.separator + trimmed)).isDirectory())
-                                cwd = new File(cwd.getAbsolutePath() + File.separator + trimmed );
-                            else 
-                                validFolder = false;
-                        }
-                        
-                        
-                    }
-                    if (validFolder){
-                        System.out.println(cwd.getAbsolutePath());
-                    }
-                    else 
-                        System.out.println("Invalid folder location.");
-                    break;
-                case "cwd":
-                    System.out.println(cwd.getAbsolutePath());
-                    break;
-                case "open":
-                    String fileName = cwd.getAbsolutePath() + File.separator + command.replace("open", "").trim();
-                    File file = new File(fileName);
-                    
-                    if (file.isFile())
-                        Desktop.getDesktop().open(file);
-                    else 
-                        System.out.println("Invalid file location.");
-                    break;
-                default: 
-                    System.out.println("Invalid command.");
-                   
-            }
-            
+            System.out.println(commandLine.execute(command));           
         }
         
         
@@ -121,53 +55,9 @@ public class Main {
         */
     }
     
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
     
-    public static String getTypeColor(String fileName){
-        int dotIndex = fileName.lastIndexOf(".");
-        if (dotIndex == -1)
-            return ANSI_BLACK;
-        
-        String type = fileName.substring(dotIndex);
-        
-        switch(type){
-            case ".jpg":
-            case ".png":
-                return ANSI_YELLOW;
-            case ".doc":
-            case ".docx":
-            case ".txt":
-            case ".rtf":
-                return ANSI_BLUE;
-            case ".pdf":
-                return ANSI_RED;
-            case ".xls":
-            case ".xlsx":
-                return ANSI_GREEN;
-            default:
-                return ANSI_BLACK;
-        }        
-   
-       
-    }
     
-    public static String parseCmd(String cmd){
-        String[] truncated = cmd.trim().split(" ");
-        for (String command : VALID_COMMANDS){
-            if (truncated[0].contains(command)){
-                return command;
-            }
-        }
-        return null;
-    }
+    
     
     
 }
